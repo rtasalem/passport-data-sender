cd12c5a723cd22e92e35ddadeda8ab6d63b7fbca
Rana Salem <ranatasalem@gmail.com>
2024-03-22 00:01:59 +0000

    refactor

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Passport Data Sender
 Simple Node.js application that send mock passport application data to a Service Bus queue.
 ## Getting Started
+Run the following command:
+```
+node app/index
+```
--- app/index.js
+++ app/index.js
@@ -1,27 +1,4 @@
 require('dotenv').config({ path: '../.env' })
-const { ServiceBusClient } = require('@azure/service-bus')
-const messages = require('./mock-data')
-
-const connectionString = process.env.ASB_CONN_STR
-const queue = process.env.ASB_QUEUE
-const sbClient = new ServiceBusClient(connectionString)
-const sender = sbClient.createSender(queue)
-
-const sendToQueue = async (message) => {
-  try {
-    const sbMessages = messages.map((msg) => {
-      return {
-        body: JSON.stringify(msg)
-      }
-    })
-    await sender.sendMessages(sbMessages)
-    console.log(`${sbMessages.length} messages sent to queue: ${queue}`)
-  } catch (error) {
-    console.error(`Error sending messages to ${queue}: `, error)
-  } finally {
-    await sender.close()
-    await sbClient.close()
-  }
-}
+const sendToQueue = require('./send-to-queue')
 
 sendToQueue()
