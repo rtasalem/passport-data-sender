47551256546e711875f5889061ca16c5127720dc
Rana Salem <ranatasalem@gmail.com>
2024-04-01 19:31:29 +0100

    remove comments from index

--- app/index.js
+++ app/index.js
@@ -1,6 +1,6 @@
 require('dotenv').config({ path: '../.env' })
-// const sendToQueue = require('./send-messages/send-to-queue')
+const sendToQueue = require('./send-messages/send-to-queue')
 const sendBatchToQueue = require('./send-messages-batch/send-batch')
 
-// sendToQueue()
+sendToQueue()
 sendBatchToQueue()
